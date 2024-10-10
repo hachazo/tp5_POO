697c277922d7569e4cd38986d7f18fbe51fe5bfc
hachazo <87842940+hachazo@users.noreply.github.com>
2024-10-10 17:04:33 -0300

    Initial commit

---
+++ punto1/torta.py
@@ -0,0 +1,13 @@
+class torta:
+    def __init__(self):
+        self.__masa = ""
+        self.__relleno = ""
+        
+    def set_masa(self, masa):
+        self.__masa = masa
+    
+    def set_relleno(self, relleno):
+        self.__relleno = relleno
+        
+    def __str__(self):
+        return f"Torta con masa de: {self.__masa}  y relleno: {self.__relleno}"
